cb4eb79537b24c47615bf679c4a4ca2822089b6a
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-07-28 19:19:49 -0300

    Correções de issues no lint [Requisito 2]

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { emailState } = this.props;
+    const INITIAL_EXPANSE = 0;
     return (
-      <div>Header</div>
+      <header>
+        <p
+          data-testid="email-field"
+        >
+          Email:
+          { ` ${emailState}` }
+        </p>
+        <p
+          data-testid="total-field"
+        >
+          Despesas: R$
+          { ` ${INITIAL_EXPANSE}` }
+        </p>
+        <p
+          data-testid="header-currency-field"
+        >
+          BRL
+        </p>
+      </header>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  emailState: state.user.email,
+});
+
+Header.propTypes = {
+  emailState: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
